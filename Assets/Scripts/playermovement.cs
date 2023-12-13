1fb828cc647bce421b9b3b01098015af1b5985c6
Jesper Heese <JesperHeese@outlook.com>
2023-12-13 11:04:06 +0100

    Added jumping and checking if grounded

--- Assets/Scripts/playermovement.cs
+++ Assets/Scripts/playermovement.cs
@@ -3,22 +3,28 @@ using UnityEngine;
 public class Playermovement : MonoBehaviour
 {
     [SerializeField] private float speed; // Speed multiplier
+    [SerializeField] private float jumpForce; // Jump force multiplier
     private Rigidbody rb; // Rigidbody component cache
     private Vector3 moveDirection; // Stores the direction of movement
     public Transform orientation; // Stores the orientation of the player
     private float verticalInput;
     private float horizontalInput;
+    private float jumpInput;
     private Camera cam;
+
+    private bool isGrounded;
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         cam = Camera.main;
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
     private void Update()
     {
         Inputs();
         RotateToCam();
+        CheckIfGrounded();
     }
     private void FixedUpdate()
     {
@@ -28,15 +34,31 @@ public class Playermovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        jumpInput = Input.GetAxisRaw("Jump");
     }
     private void Movement()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection * speed, ForceMode.Force);
+        if (jumpInput > 0 && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
     private void RotateToCam()
     {
         transform.rotation = Quaternion.Euler(0f, cam.transform.rotation.eulerAngles.y, 0f);
+    }
+    private void CheckIfGrounded()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit raycast, 1.1f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }
 
